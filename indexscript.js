e90325f37b0b0dc0fc0c12f91cdd65de7470746a
denis cheremnykh <denischeremnykh@mail.ru>
2024-06-07 16:06:31 +0300

    about page adaptive, minor fixes on both pages

--- indexscript.js
+++ indexscript.js
@@ -1,11 +1,13 @@
 const burgerButton = document.querySelector('.header__burger');
 const burgerMenu = document.querySelector('.burger-menu');
+let isOpen = false
 
 burgerButton.addEventListener('click' , () => {
-
-    if(burgerMenu.style.display === 'none'){
-        burgerMenu.style.display = 'flex';
+    if(!isOpen){
+        burgerMenu.style.display = 'block';
+        isOpen = true
     }else{
         burgerMenu.style.display = 'none';
+        isOpen = false
     }
 });
